fc76734466b8c78a7edf59d79dad952d7f8e2609
Daniel Lu <daniel_lu@hotmail.com>
2022-01-22 12:07:30 -0500

    add src/helper file and add selectors.js file that contains helper function and add getAppointmentForDay

---
+++ src/helpers/selectors.js
@@ -0,0 +1,18 @@
+export function getAppointmentsForDay(state, day) {
+  const appointmentArr = [];
+  
+  for (let stateDay of state.days) {
+    if (stateDay.name === day) {
+      for (let appID of stateDay.appointments) {
+        for (let key in state.appointments) {
+          if (appID.toString() === key) {
+            appointmentArr.push(state.appointments[key])
+          }
+        }
+      }
+    }
+  };
+  
+  
+  return appointmentArr;
+}
