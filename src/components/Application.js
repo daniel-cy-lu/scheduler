7a0b6831cde4ff05746a676e0734031bc89273ca
Daniel Lu <daniel_lu@hotmail.com>
2022-01-22 20:45:58 -0500

    use Promise.all to make requests to all three api, then use the res to setState at the same time. Also use getAppointmentsForDay helper and save results in dailyAppointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,25 +4,29 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {} 
+    appointments: {}
   });
   
-  const dailyAppointments = [];
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
   const setDay = day => setState({...state, day});
-  const setDays = days => setState(prev => ({...prev, days}));
 
   const parsedAppointments = dailyAppointments.map(appointment => <Appointment {...appointment} key={appointment.id} /> )
 
   useEffect(() => {
-    axios.get("api/days")
-    .then((res) => {
-      setDays(res.data);
-    })
+    Promise.all([
+      axios.get("api/days"),
+      axios.get("api/appointments"),
+      axios.get("api/interviewers")
+    ])
+    .then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+    });
   }, [])
 
   return (
