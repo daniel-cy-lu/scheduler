2c79f85d18f80c3c90ada6bd01e30b43548f3b7b
Daniel Lu <daniel_lu@hotmail.com>
2022-01-22 19:55:47 -0500

    combine state in application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -49,8 +49,14 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday')
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {} 
+  });
+
+  const setDay = day => setState({...state, day});
+  const setDays = days => setState(prev => ({...prev, days}));
 
   const parsedAppointments = appointments.map(appointment => <Appointment {...appointment} key={appointment.id} /> )
 
@@ -73,8 +79,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList 
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
            
           />
