1ad568c14eb5515472f712fdaf600043ee9a38a0
Daniel Lu <daniel_lu@hotmail.com>
2022-01-25 15:48:19 -0500

    add SAVING mode in Appointment component. To let user know the appointment is being saved

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,10 +5,12 @@ import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
 import useVisualMode from "hooks/useVisualMode";
+import Status from "./Status";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
@@ -18,6 +20,8 @@ export default function Appointment(props) {
       student: name,
       interviewer
     };
+    
+   transition(SAVING);
 
     props.bookInterview(props.id, interview)
     .then(() => {
@@ -37,6 +41,7 @@ export default function Appointment(props) {
         />
        )}
        {mode === CREATE && <Form interviewers={props.interviewers} onCancel={() => back(EMPTY)} onSave={save} />}
+       {mode === SAVING && <Status message={"Saving"}/>}
     </article>
   )
 }
